49f39f4c442a3226e2c79c19b804bbb6135b0d3e
santosh <santosh@MYPC>
2018-10-11 11:17:30 +0530

    Profile view

--- LetsCookApp/LetsCookApp/Views/MyProfileView.xaml.cs
+++ LetsCookApp/LetsCookApp/Views/MyProfileView.xaml.cs
@@ -17,5 +17,34 @@ namespace LetsCookApp.Views
             InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, false);
         }
+
+        
+        private void Timeline_Tapped(object sender, EventArgs e)
+        {
+            TLine.IsVisible =grdTimeline.IsVisible= true;
+            ALine.IsVisible = FLine.IsVisible = GLine.IsVisible = false;
+            grdAboutme.IsVisible = grdFriends.IsVisible = grdGallery.IsVisible = false;
+        }
+        private void AboutMe_Tapped(object sender, EventArgs e)
+        {
+            ALine.IsVisible =grdAboutme.IsVisible= true;
+            TLine.IsVisible = FLine.IsVisible = GLine.IsVisible = false;
+            grdTimeline.IsVisible = grdFriends.IsVisible = grdGallery.IsVisible = false;
+
+        }
+        private void Friends_Tapped(object sender, EventArgs e)
+        {
+            FLine.IsVisible = grdFriends.IsVisible= true;
+            TLine.IsVisible = ALine.IsVisible = GLine.IsVisible = false;
+            grdTimeline.IsVisible = grdFriends.IsVisible = grdGallery.IsVisible = false;
+        }
+        private void Gallery_Tapped(object sender, EventArgs e)
+        {
+            GLine.IsVisible =grdGallery.IsVisible= true;
+            TLine.IsVisible = FLine.IsVisible = ALine.IsVisible = false;
+            grdTimeline.IsVisible = grdFriends.IsVisible = grdAboutme.IsVisible = false;
+        }
     }
+
+
 }
