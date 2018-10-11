33f39c5e52148d5d2ae0028a6705d88113bd0339
santosh <santosh@MYPC>
2018-10-12 00:39:28 +0530

    profile view

--- LetsCookApp/LetsCookApp/Views/MyProfileView.xaml.cs
+++ LetsCookApp/LetsCookApp/Views/MyProfileView.xaml.cs
@@ -14,8 +14,17 @@ namespace LetsCookApp.Views
     {
         public MyProfileView()
         {
+           
+
+            
             InitializeComponent();
+            
             NavigationPage.SetHasNavigationBar(this, false);
+            TLine.IsVisible = grdTimeline.IsVisible = true;
+            ALine.IsVisible = FLine.IsVisible = GLine.IsVisible = false;
+            grdAboutme.IsVisible = listFriends.IsVisible = grdGallery.IsVisible = false;
+
+
         }
 
         
@@ -23,26 +32,34 @@ namespace LetsCookApp.Views
         {
             TLine.IsVisible =grdTimeline.IsVisible= true;
             ALine.IsVisible = FLine.IsVisible = GLine.IsVisible = false;
-            grdAboutme.IsVisible = grdFriends.IsVisible = grdGallery.IsVisible = false;
+            grdAboutme.IsVisible = listFriends.IsVisible = grdGallery.IsVisible = false;
         }
         private void AboutMe_Tapped(object sender, EventArgs e)
         {
             ALine.IsVisible =grdAboutme.IsVisible= true;
             TLine.IsVisible = FLine.IsVisible = GLine.IsVisible = false;
-            grdTimeline.IsVisible = grdFriends.IsVisible = grdGallery.IsVisible = false;
+            grdTimeline.IsVisible = listFriends.IsVisible = grdGallery.IsVisible = false;
 
         }
         private void Friends_Tapped(object sender, EventArgs e)
         {
-            FLine.IsVisible = grdFriends.IsVisible= true;
+            FLine.IsVisible = listFriends.IsVisible= true;
             TLine.IsVisible = ALine.IsVisible = GLine.IsVisible = false;
-            grdTimeline.IsVisible = grdFriends.IsVisible = grdGallery.IsVisible = false;
+            grdTimeline.IsVisible = grdAboutme.IsVisible = grdGallery.IsVisible = false;
+            List<SubCategory> _listAvailableAward = new List<SubCategory>()
+            {
+                new SubCategory {foodIcon = "cacke.png" ,DishName = "Santosh", likeIcon = "icon.png" ,timeIcon = "icon.png" ,Time = "9 MIn", servingIcon = "icon.png", Servings="6 Servings",ingrendIcon="icon.png" , Ingrendients="Team Lead" ,plusIcon="icon.png"},
+                new SubCategory {foodIcon = "donat.png" ,DishName = "Dipika", likeIcon = "icon.png" ,timeIcon = "icon.png" ,Time = "30 MIn", servingIcon = "icon.png", Servings="4 Servings",ingrendIcon="icon.png" , Ingrendients="Graphics Designer" ,plusIcon="icon.png"},
+                new SubCategory {foodIcon = "ruge.png" ,DishName = "John", likeIcon = "icon.png" ,timeIcon = "icon.png" ,Time = "15 MIn", servingIcon = "icon.png", Servings="4 Servings",ingrendIcon="icon.png" , Ingrendients="Tester" ,plusIcon="icon.png"},
+            };
+            listFriends.ItemsSource = _listAvailableAward;
+
         }
         private void Gallery_Tapped(object sender, EventArgs e)
         {
             GLine.IsVisible =grdGallery.IsVisible= true;
             TLine.IsVisible = FLine.IsVisible = ALine.IsVisible = false;
-            grdTimeline.IsVisible = grdFriends.IsVisible = grdAboutme.IsVisible = false;
+            grdTimeline.IsVisible = listFriends.IsVisible = grdAboutme.IsVisible = false;
         }
     }
 
